c7680ceeaca8d10161bb0082380137d46f22829f
Terrence Chung <terrencechung35@gmail.com>
2023-01-10 18:30:54 -0800

    lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const drivers = (['Antonia', 'Nuru', 'Amari', 'Mo'])
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMultiplier){
+    return function(value){
+        return fareMultiplier * value;
+    }
+}
+
+const fareDoubler = function(fareDoubler){
+    return fareDoubler * 2;
+}
+
+const fareTripler = function(fareTripler){
+    return fareTripler * 3;
+}
+
+function selectDifferentDrivers(driversArray, fn) {
+    return fn(driversArray);
+}
